69b770285233c81236f706e2bfd2fe63faf752ff
raphael martins <raphael@lolo.net.br>
2016-10-24 22:44:39 -0200

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var PROD = process.env.NODE_ENV == "production";
+
+module.exports = {
+  entry:
+  {
+    app:'./src/js/app.js'
+  },
+  plugins: PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ] : [],
+
+  output: {
+    filename: '[name].js',
+    publicPath: ''
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react&plugins[]=transform-object-rest-spread' }
+    ]
+  }
+}
