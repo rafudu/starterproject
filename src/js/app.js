45bdd1dff7b23d0044aec52323712459bfc8dcbc
raphael martins <raphael@lolo.net.br>
2016-12-29 12:51:28 -0200

    Flow test

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 document.getElementById("message").innerHTML = "Looks like we're all set!";
 
 const changeColors = function(){
-  const elements: Array<HTMLElement> = Array.prototype.slice.call(document.getElementsByClassName("color"));
+  const elements: Array<HTMLElement> = Array.prototype.slice.call(document.getElementsByClassName("color")); // Little exagerated but just to test the flow plugins.
     elements.forEach((elm) => {
       elm.style.backgroundColor = 'hsl(' + Math.round(Math.random()*360) + ',' + 100 + '%,' + 50 + '%)';
     })
